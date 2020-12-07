2b123d98915bf0f3408e8fbb99b4a30ecee8e31d
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2020-12-07 20:39:14 +0400

    Add database simple integration tests - closes #7

--- src/main/java/nc/unc/cs/Project2020Application.java
+++ src/main/java/nc/unc/cs/Project2020Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Project2020Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Project2020Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Project2020Application.class, args);
+    }
 
 }
