f7f5682eadb5dd7333f7bba45a28366988dcb82e
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-01-22 17:42:40 +0400

    Use OkHttp as HTTP client for Feign-based services + add timeout = 5s

---
+++ services/common/src/main/java/nc/unc/cs/services/common/services/HttpClientConfiguration.java
@@ -0,0 +1,20 @@
+package nc.unc.cs.services.common.services;
+
+import java.util.concurrent.TimeUnit;
+import feign.okhttp.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpClientConfiguration {
+
+    @Bean
+    public OkHttpClient client() {
+        okhttp3.OkHttpClient client =
+            new Builder()
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .build();
+        return new OkHttpClient(client);
+    }
+}
