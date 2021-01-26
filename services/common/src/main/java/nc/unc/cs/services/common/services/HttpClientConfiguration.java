49cb092f3e063dea1607faf9788825783d6555b9
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-01-26 18:59:02 +0400

    logging of the httpClientRequests

--- services/common/src/main/java/nc/unc/cs/services/common/services/HttpClientConfiguration.java
+++ services/common/src/main/java/nc/unc/cs/services/common/services/HttpClientConfiguration.java
@@ -1,20 +1,20 @@
-//package nc.unc.cs.services.common.services;
-//
-//import java.util.concurrent.TimeUnit;
-//import feign.okhttp.OkHttpClient;
-//import okhttp3.OkHttpClient.Builder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class HttpClientConfiguration {
-//
-//    @Bean
-//    public OkHttpClient client() {
-//        okhttp3.OkHttpClient client =
-//            new Builder()
-//                .connectTimeout(2, TimeUnit.SECONDS)
-//                .build();
-//        return new OkHttpClient(client);
-//    }
-//}
+package nc.unc.cs.services.common.services;
+
+import java.util.concurrent.TimeUnit;
+import feign.okhttp.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpClientConfiguration {
+
+    @Bean
+    public OkHttpClient client() {
+        okhttp3.OkHttpClient client =
+            new Builder()
+                .connectTimeout(2, TimeUnit.SECONDS)
+                .build();
+        return new OkHttpClient(client);
+    }
+}
