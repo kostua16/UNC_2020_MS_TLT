2b8f8ec87d2b4008fec20159f214ea7184e9f12c
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-02-07 18:20:13 +0400

    Documentation for UtilitiesPriceListController #17

--- services/communal/src/main/java/nc/unc/cs/services/communal/controllers/UtilitiesPriceListController.java
+++ services/communal/src/main/java/nc/unc/cs/services/communal/controllers/UtilitiesPriceListController.java
@@ -1,6 +1,11 @@
 package nc.unc.cs.services.communal.controllers;
 
 import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import nc.unc.cs.services.communal.entities.UtilitiesPriceList;
 import nc.unc.cs.services.communal.services.CommunalService;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "communal/utilities/price")
 @CrossOrigin
+@Api(value = "Utilities Price List API")
 public class UtilitiesPriceListController {
 
     private final CommunalService communalService;
@@ -23,6 +29,24 @@ public class UtilitiesPriceListController {
     }
 
     @PostMapping(consumes = "application/json", produces = "application/json")
+    @ApiOperation(
+        httpMethod = "POST",
+        value = "Creating and adding price lists",
+        notes = "Creation and addition of price lists for calculating the amount of spent utilities.",
+        nickname = "addUtilitiesPriceList"
+    )
+    @ApiResponses({
+        @ApiResponse(code = 400, message = "UtilitiesPriceList with ID = null", response = UtilitiesPriceList.class)
+    })
+    @ApiImplicitParam(
+        name = "utilitiesPriceList",
+        value = "Data for registration of the price list",
+        required = true,
+        type = "UtilitiesPriceList",
+        dataType = "UtilitiesPriceList",
+        dataTypeClass = UtilitiesPriceList.class,
+        paramType = "body"
+    )
     public ResponseEntity<UtilitiesPriceList> addUtilitiesPriceList(
         @RequestBody final UtilitiesPriceList utilitiesPriceList
     ) {
@@ -30,6 +54,11 @@ public class UtilitiesPriceListController {
     }
 
     @GetMapping(produces = "application/json")
+    @ApiOperation(
+        httpMethod = "GET",
+        value = "List of UtilitiesPriceList",
+        nickname = "getAllUtilitiesPriceList"
+    )
     public List<UtilitiesPriceList> getAllUtilitiesPriceList() {
         return this.communalService.getAllUtilitiesPriceList();
     }
