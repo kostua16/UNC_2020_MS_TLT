31511309d5987a5857d47b5bf3a9908d98eb2253
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-03-02 18:01:06 +0400

    feature: tests for checking errors in string data. (reopen) #84

---
+++ services/communal/src/test/java/nc/unc/cs/services/communal/controllers/mock/registration/IncorrectRegion.java
@@ -0,0 +1,56 @@
+package nc.unc.cs.services.communal.controllers.mock.registration;
+
+import nc.unc.cs.services.communal.controllers.mock.IncorrectDataTests;
+import nc.unc.cs.services.communal.controllers.mock.PropertyAndRegistrationParentWebTest;
+import nc.unc.cs.services.communal.entities.Property;
+import nc.unc.cs.services.communal.entities.Registration;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class IncorrectRegion extends PropertyAndRegistrationParentWebTest implements IncorrectDataTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(IncorrectRegion.class);
+
+    @Test
+    @Override
+    public void blankData() throws Exception {
+        Registration registration = this.createRegistration();
+        registration.setRegion("  ");
+
+        this.mockMvc.perform(post(REGISTRATION_CONTROLLER_MAPPING + "/property/add")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(registration)))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Override
+    public void smallestSizeData() throws Exception {
+        Registration registration = this.createRegistration();
+        registration.setRegion(" 1 ");
+
+        this.mockMvc.perform(post(REGISTRATION_CONTROLLER_MAPPING + "/property/add")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(registration)))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Override
+    public void oversizeData() throws Exception {
+        Registration registration = this.createRegistration();
+        registration.setRegion("asdsdsdgqsr1rvxzxgadgasfasfasdasdasdasdasdasdasdasdasdasd");
+
+        this.mockMvc.perform(post(REGISTRATION_CONTROLLER_MAPPING + "/property/add")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(registration)))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+}
