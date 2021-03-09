1d819a621e4111bbe0238f58c447c07a80a53c04
Restyled.io <commits@restyled.io>
2021-03-09 11:47:36 +0000

    Restyled by google-java-format

--- services/common/src/main/java/nc/unc/cs/services/common/actuator/DiscoveryServices.java
+++ services/common/src/main/java/nc/unc/cs/services/common/actuator/DiscoveryServices.java
@@ -32,8 +32,7 @@ public class DiscoveryServices {
   private final List<Integer> correctCodes = Arrays.asList(200, 401, 403);
 
   @Autowired
-  public DiscoveryServices(final EurekaClient client,
-                           final OkHttpClient httpClient) {
+  public DiscoveryServices(final EurekaClient client, final OkHttpClient httpClient) {
     this.client = client;
     this.httpClient = httpClient;
   }
@@ -47,9 +46,7 @@ public class DiscoveryServices {
   public List<String> serviceInfo(@Selector String name) throws IOException {
     List<String> result = new ArrayList<>();
     final Optional<Application> service =
-        this.client.getApplications()
-            .getRegisteredApplications()
-            .stream()
+        this.client.getApplications().getRegisteredApplications().stream()
             .filter(app -> name.equals(app.getName()))
             .findFirst();
 
@@ -57,23 +54,25 @@ public class DiscoveryServices {
       String current = "";
       final Application application = service.get();
       for (InstanceInfo instance : application.getInstances()) {
-        log.error(String.format("Testing %s - %s...", application.getName(),
-                                instance.getId()));
+        log.error(String.format("Testing %s - %s...", application.getName(), instance.getId()));
         try {
-          final Call call = this.httpClient.newCall(
-              new Request.Builder()
-                  .get()
-                  .url(new URL(instance.getHealthCheckUrl()))
-                  .build());
+          final Call call =
+              this.httpClient.newCall(
+                  new Request.Builder().get().url(new URL(instance.getHealthCheckUrl())).build());
           final Response response = call.execute();
-          current = String.format("[%s = %s, Code: %d, Success: %b]",
-                                  instance.getId(), instance.getHostName(),
-                                  response.code(), response.isSuccessful());
+          current =
+              String.format(
+                  "[%s = %s, Code: %d, Success: %b]",
+                  instance.getId(),
+                  instance.getHostName(),
+                  response.code(),
+                  response.isSuccessful());
 
         } catch (IOException exception) {
           log.error("Test failed", exception);
-          String.format("[%s = %s, Fail: %s]", instance.getId(),
-                        instance.getHostName(), exception.getMessage());
+          String.format(
+              "[%s = %s, Fail: %s]",
+              instance.getId(), instance.getHostName(), exception.getMessage());
         }
         result.add(current);
         log.error(current);
