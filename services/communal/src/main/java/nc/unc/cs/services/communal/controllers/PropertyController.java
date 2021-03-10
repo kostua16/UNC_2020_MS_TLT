8dc882d123eac1ef1d11655d97e23ca680159db3
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-03-10 17:12:36 +0400

    fix: Fixed bug in PropertyController. closes #159

--- services/communal/src/main/java/nc/unc/cs/services/communal/controllers/PropertyController.java
+++ services/communal/src/main/java/nc/unc/cs/services/communal/controllers/PropertyController.java
@@ -1,21 +1,21 @@
 package nc.unc.cs.services.communal.controllers;
 
-import io.swagger.annotations.Api;
 import java.util.List;
+import io.swagger.annotations.Api;
 import nc.unc.cs.services.communal.controllers.payloads.CreationProperty;
 import nc.unc.cs.services.communal.entities.Property;
 import nc.unc.cs.services.communal.services.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("communal/property")
 @Api(value = "Property API")
 public class PropertyController {
@@ -29,13 +29,15 @@ public class PropertyController {
 
     @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<Property> addCitizensProperty(
-            @Validated @RequestBody final CreationProperty newProperty) {
+        @Validated @RequestBody final CreationProperty newProperty) {
         return this.registrationService.addCitizensProperty(newProperty);
     }
 
-    @GetMapping(value = "property/citizen/{citizenId}", produces = "application/json")
+    @GetMapping(value = "citizen/{citizenId}", produces = "application/json")
     public List<Property> getPropertiesByCitizenId(
-            @PathVariable("citizenId") final Long citizenId) {
+        @PathVariable("citizenId") final Long citizenId
+    ) {
+        System.out.println("WORK!");
         return this.registrationService.getPropertiesByCitizenId(citizenId);
     }
 }
