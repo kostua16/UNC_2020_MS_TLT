8add45eb5c9411d3127f58797105cb0c966fb919
Restyled.io <commits@restyled.io>
2021-03-13 08:41:02 +0000

    Restyled by google-java-format

--- services/bank/src/main/java/nc/unc/cs/services/bank/entities/Transaction.java
+++ services/bank/src/main/java/nc/unc/cs/services/bank/entities/Transaction.java
@@ -1,5 +1,7 @@
 package nc.unc.cs.services.bank.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +11,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,42 +26,41 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class Transaction {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long transactionId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long transactionId;
 
-    @Column(nullable = false, updatable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    @Temporal(value = TemporalType.TIMESTAMP)
-    @NotNull
-    private Date creationDate;
+  @Column(nullable = false, updatable = false)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  @Temporal(value = TemporalType.TIMESTAMP)
+  @NotNull
+  private Date creationDate;
 
-    @NotNull
-    @Min(value = 1, message = "Incorrect payment amount")
-    @Column(nullable = false, updatable = false)
-    private Integer amount;
+  @NotNull
+  @Min(value = 1, message = "Incorrect payment amount")
+  @Column(nullable = false, updatable = false)
+  private Integer amount;
 
-    @NotNull
-    @Min(value = 1L, message = "Incorrect payment request ID")
-    @Column(nullable = false, updatable = false)
-    private Long paymentRequestId;
+  @NotNull
+  @Min(value = 1L, message = "Incorrect payment request ID")
+  @Column(nullable = false, updatable = false)
+  private Long paymentRequestId;
 
-    @NotNull
-    @Min(value = 1L, message = "Incorrect citizen ID")
-    @Column(nullable = false, updatable = false)
-    private Long citizenId;
+  @NotNull
+  @Min(value = 1L, message = "Incorrect citizen ID")
+  @Column(nullable = false, updatable = false)
+  private Long citizenId;
 
-    @Builder
-    public Transaction(
-        final Long transactionId,
-        final Integer amount,
-        final Long paymentRequestId,
-        final Long citizenId
-    ) {
-        this.transactionId = transactionId;
-        this.creationDate = new Date();
-        this.amount = amount;
-        this.paymentRequestId = paymentRequestId;
-        this.citizenId = citizenId;
-    }
+  @Builder
+  public Transaction(
+      final Long transactionId,
+      final Integer amount,
+      final Long paymentRequestId,
+      final Long citizenId) {
+    this.transactionId = transactionId;
+    this.creationDate = new Date();
+    this.amount = amount;
+    this.paymentRequestId = paymentRequestId;
+    this.citizenId = citizenId;
+  }
 }
