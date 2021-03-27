30ce227fa7001e2424bc85ea0ccd190f1be93203
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-03-27 20:18:22 +0400

    feature: added a panel with notifications about payment requests #38

---
+++ client-ui/src/components/main/PaymentRequestItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{paymentRequest.paymentRequestId}}</p>
+    <p>{{paymentRequest.amount}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "PaymentRequestItem",
+  props: ['paymentRequest'],
+
+  created() {
+    console.log(this.paymentRequest)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
