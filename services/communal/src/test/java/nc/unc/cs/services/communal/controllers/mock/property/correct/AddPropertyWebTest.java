7cca7a63e1e93ccb5891cec0e2023a3e10b6bf61
ZabegaevN <zabegaev99@list.ru>
2021-03-28 16:03:24 +0400

    feature: add Property documentation

--- services/communal/src/test/java/nc/unc/cs/services/communal/controllers/mock/property/correct/AddPropertyWebTest.java
+++ services/communal/src/test/java/nc/unc/cs/services/communal/controllers/mock/property/correct/AddPropertyWebTest.java
@@ -2,6 +2,9 @@ package nc.unc.cs.services.communal.controllers.mock.property.correct;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -14,10 +17,24 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
 class AddPropertyWebTest extends PropertyParentWeb {
 
     private static final Logger logger = LoggerFactory.getLogger(AddPropertyWebTest.class);
+
+  private static final FieldDescriptor ADD_REGION_DESCR =
+      fieldWithPath("region").type(String.class).description("region where the property is located");
+
+  private static final FieldDescriptor ADD_CITY_DESCR =
+      fieldWithPath("city").type(String.class).description("city where the property is located");
+
+  private static final FieldDescriptor[] PROPERTY_DESCR =
+      new FieldDescriptor[] {AddPropertyWebTest.ADD_REGION_DESCR, AddPropertyWebTest.ADD_CITY_DESCR};
+
+  private static final ResponseFieldsSnippet PROPERTY_RESP = responseFields(AddPropertyWebTest.PROPERTY_DESCR);
+
 
     @Test
     void addCitizensPropertyTest() throws Exception {
@@ -33,6 +50,7 @@ class AddPropertyWebTest extends PropertyParentWeb {
                         post(PROPERTY_CONTROLLER_MAPPING)
                                 .contentType("application/json")
                                 .content(objectMapper.writeValueAsString(creationProperty)))
+                .andDo(document("addCitizensProperty", PROPERTY_RESP))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(
