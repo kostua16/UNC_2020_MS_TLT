53d2e7f3db0aaed7ffd80dd9ebf93247a65c12e8
ZabegaevN <zabegaev99@list.ru>
2021-03-29 20:26:24 +0400

    feature: updated tests #236

---
+++ services/passport/src/test/java/nc/unc/cs/services/passport/controller/UpdateDomesticTest.java
@@ -0,0 +1,71 @@
+package nc.unc.cs.services.passport.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nc.unc.cs.services.passport.controller.dto.DomesticDTO;
+import nc.unc.cs.services.passport.model.Citizen;
+import nc.unc.cs.services.passport.model.Domestic;
+import nc.unc.cs.services.passport.service.PassportTable;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {PassportController.class})
+//@Import(ObjectMapper.class)
+//@WithMockUser("customUsername") // аннотация для создания авторизованного пользователя
+class UpdateDomesticTest {
+  private static final String PASSPORT_CONTROLLER_MAPPING = "http://localhost:8095";
+  private static final Logger logger = LoggerFactory.getLogger(UpdateDomesticTest.class);
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private PassportTable passportTable;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Test
+  void updateDomesticPassportTest() throws Exception {
+    Citizen citizen = new Citizen();
+    citizen.setCitizenId(111L);
+    citizen.setSurname("Pupkin");
+    citizen.setName("Vasya");
+    citizen.setRegistration("Samara");
+    citizen.setDateOfBirth(new Date());
+
+    DomesticDTO domesticDTO = new DomesticDTO(
+        1L,
+        "Samara",
+        "Pupkin",
+        "Vasya",
+        citizen.getDateOfBirth(),
+        false,
+        2222,
+        333333,
+        111L
+    );
+
+    final Domestic domestic = new Domestic();
+
+    when(passportTable.updateDomestic(domesticDTO.getDomesticId(), domesticDTO))
+    .thenReturn(ResponseEntity.ok(domestic));
+
+
+    mockMvc.perform(post(PASSPORT_CONTROLLER_MAPPING + "/passport/updateDomestic/" + domesticDTO.getDomesticId())
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(domesticDTO)))
+        .andExpect(status().isOk());
+  }
+}
---
+++ services/passport/src/test/java/nc/unc/cs/services/passport/service/ActivateInternationalTest.java
@@ -0,0 +1,61 @@
+package nc.unc.cs.services.passport.service;
+
+import nc.unc.cs.services.common.clients.bank.BankService;
+import nc.unc.cs.services.passport.controller.dto.InternationalDTO;
+import nc.unc.cs.services.passport.model.International;
+import nc.unc.cs.services.passport.repository.InternationalRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class ActivateInternationalTest {
+
+
+  @InjectMocks
+  private PassportTable passportTable;
+
+  @Mock
+  private BankService bankService;
+
+  @Mock
+  private InternationalRepository internationalRepository;
+
+  private International createInternational() {
+    return International
+        .builder()
+        .internationalId(1L)
+        .locked(false)
+        .name("Nikita")
+        .surname("Sss")
+        .dateOfBirth(new Date(5555))
+        .isActive(false)
+        .citizenId(111L)
+        .build();
+
+  }
+
+  @Test
+  void getInternationalPassport() throws Exception {
+
+    final International international = this.createInternational();
+
+    when(this.internationalRepository.findById(international.getInternationalId()))
+        .thenReturn(Optional.of(international));
+    when(this.internationalRepository.save(international))
+        .thenReturn(international);
+
+    final International response = this.passportTable.activateInternational(international.getInternationalId());
+
+    Assertions.assertTrue(response.getIsActive());
+  }
+
+}
