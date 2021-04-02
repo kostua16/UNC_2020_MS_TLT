8708a12ed36c6fac754f4312aa48020776e47e32
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-04-02 16:31:07 +0400

    feature: added tests #214

---
+++ services/account/src/test/java/nc/unc/cs/services/account/controllers/LoginWebTest.java
@@ -0,0 +1,85 @@
+package nc.unc.cs.services.account.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nc.unc.cs.services.account.controllers.dto.LoginDto;
+import nc.unc.cs.services.account.services.AuthService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {AuthController.class})
+@Import(ObjectMapper.class)
+class LoginWebTest {
+  private static final String LOGIN_CONTROLLER_MAPPING = "http://localhost:8888/auth/sign-in";
+
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private AuthService authService;
+
+  private LoginDto createLoginDto() {
+    return LoginDto
+        .builder()
+        .username("username")
+        .password("password")
+        .build();
+  }
+
+  @Test
+  void loginSuccessfulTest() throws Exception {
+    final LoginDto loginDto = this.createLoginDto();
+
+    given(this.authService.login(loginDto)).willReturn(ResponseEntity.ok(1L));
+
+    this.mockMvc
+        .perform(
+            post(LOGIN_CONTROLLER_MAPPING)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(loginDto)))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"   ", "sss ", "sssssssssssssssssssssssssssssssssssssssss"})
+  void invalidUsernameLoginTest(final String word) throws Exception {
+    final LoginDto loginDto = this.createLoginDto();
+    loginDto.setUsername(word);
+
+    this.mockMvc
+        .perform(
+            post(LOGIN_CONTROLLER_MAPPING)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(loginDto)))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"   ", "ssss", "sssssssssssssssssssssssssssssssssssssssss"})
+  void invalidPasswordLoginTest(final String word) throws Exception {
+    final LoginDto loginDto = this.createLoginDto();
+    loginDto.setPassword(word);
+
+    this.mockMvc
+        .perform(
+            post(LOGIN_CONTROLLER_MAPPING)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(loginDto)))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
+}
---
+++ services/account/src/test/java/nc/unc/cs/services/account/controllers/RegistrationWebTest.java
@@ -0,0 +1,59 @@
+package nc.unc.cs.services.account.controllers;
+
+import java.util.Date;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nc.unc.cs.services.account.controllers.dto.RegistrationDto;
+import nc.unc.cs.services.account.services.AuthService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.ArgumentMatchers.longThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {AuthController.class})
+@Import(ObjectMapper.class)
+class RegistrationWebTest {
+  private static final String REGISTER_CONTROLLER_MAPPING = "http://localhost:8888/auth/sign-up";
+
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private AuthService authService;
+
+  private RegistrationDto createRegistrationDto() {
+    return RegistrationDto
+        .builder()
+        .username("username")
+        .password("password")
+        .name("name")
+        .surname("surname")
+        .dateOfBirth(new Date())
+        .registration("samara")
+        .build();
+  }
+
+  @Test
+  void registerSuccessfulTest() throws Exception {
+    final RegistrationDto registrationDto = this.createRegistrationDto();
+    given(this.authService.register(registrationDto))
+        .willReturn(ResponseEntity.ok(registrationDto.getUsername()));
+
+    this.mockMvc
+        .perform(
+            post(REGISTER_CONTROLLER_MAPPING)
+                .contentType("application/json\"")
+                .content(objectMapper.writeValueAsString(registrationDto)))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+}
