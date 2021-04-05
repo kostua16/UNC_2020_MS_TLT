6b03bd1349a605df59313e9a52102647f8737c0a
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-04-05 20:22:11 +0400

    feature: added web tests for bank close #87
    # Conflicts:
    #	services/bank/src/test/java/nc/unc/cs/services/bank/controllers/GetInfoTest.java
    #	services/bank/src/test/java/nc/unc/cs/services/bank/controllers/PayTest.java
    #	services/bank/src/test/java/nc/unc/cs/services/bank/controllers/RequestPaymentTest.java

--- services/bank/src/test/java/nc/unc/cs/services/bank/controllers/GetInfoTest.java
+++ services/bank/src/test/java/nc/unc/cs/services/bank/controllers/GetInfoTest.java
@@ -1,14 +1,9 @@
 package nc.unc.cs.services.bank.controllers;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nc.unc.cs.services.bank.entities.PaymentRequest;
 import nc.unc.cs.services.bank.services.BankService;
 import org.junit.jupiter.api.Test;
@@ -17,35 +12,47 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {BankController.class})
 @Import(ObjectMapper.class)
-class GetInfoTest {
+public class GetInfoTest {
   private static final String BANK_CONTROLLER_MAPPING = "http://localhost:8084/bank/";
-  @Autowired private MockMvc mockMvc;
-  @MockBean private BankService bankService;
+  @Autowired
+  private MockMvc mockMvc;
+  @MockBean
+  private BankService bankService;
 
   @Test
   void checkPaymentStatusTest() throws Exception {
     final Long paymentId = 1L;
-    when(this.bankService.isPaid(paymentId)).thenReturn(Boolean.TRUE);
+    when(this.bankService.isPaid(paymentId))
+        .thenReturn(Boolean.TRUE);
 
     this.mockMvc
-        .perform(get(BANK_CONTROLLER_MAPPING + paymentId).contentType("application/json"))
+        .perform(
+            get(BANK_CONTROLLER_MAPPING + paymentId)
+                .contentType("application/json")
+        )
         .andDo(print())
         .andExpect(status().isOk());
   }
-
   @Test
   void getDebtPaymentRequestsTest() throws Exception {
     final List<PaymentRequest> list = new ArrayList<>();
     Collections.addAll(list, new PaymentRequest(), new PaymentRequest());
     final Long citizenId = 111L;
-    when(this.bankService.getDebtPaymentRequests(citizenId)).thenReturn(list);
+    when(this.bankService.getDebtPaymentRequests(citizenId))
+        .thenReturn(list);
 
     this.mockMvc
         .perform(
-            get(BANK_CONTROLLER_MAPPING + "check/" + citizenId).contentType("application/json"))
+            get(BANK_CONTROLLER_MAPPING + "check/" + citizenId)
+                .contentType("application/json")
+        )
         .andDo(print())
         .andExpect(status().isOk());
   }
--- services/bank/src/test/java/nc/unc/cs/services/bank/controllers/PayTest.java
+++ services/bank/src/test/java/nc/unc/cs/services/bank/controllers/PayTest.java
@@ -1,10 +1,5 @@
 package nc.unc.cs.services.bank.controllers;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nc.unc.cs.services.bank.entities.Transaction;
 import nc.unc.cs.services.bank.services.BankService;
@@ -15,18 +10,25 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {BankController.class})
 @Import(ObjectMapper.class)
 class PayTest {
   private static final String BANK_CONTROLLER_MAPPING = "http://localhost:8084/bank/payment/";
-  @Autowired private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
   //  @Autowired
-  //  private ObjectMapper objectMapper;
-  @MockBean private BankService bankService;
+//  private ObjectMapper objectMapper;
+  @MockBean
+  private BankService bankService;
 
   private Transaction createTransaction() {
-    return Transaction.builder()
+    return Transaction
+        .builder()
         .transactionId(1L)
         .citizenId(111L)
         .paymentRequestId(15L)
@@ -38,10 +40,14 @@ class PayTest {
   void payTest() throws Exception {
     final Long paymentId = 1L;
     final Transaction transaction = this.createTransaction();
-    when(this.bankService.payment(paymentId)).thenReturn(ResponseEntity.ok(transaction));
+    when(this.bankService.payment(paymentId))
+        .thenReturn(ResponseEntity.ok(transaction));
 
     this.mockMvc
-        .perform(put(BANK_CONTROLLER_MAPPING + paymentId).contentType("application/json"))
+        .perform(
+            put(BANK_CONTROLLER_MAPPING + paymentId)
+                .contentType("application/json")
+        )
         .andDo(print())
         .andExpect(status().isOk());
   }
