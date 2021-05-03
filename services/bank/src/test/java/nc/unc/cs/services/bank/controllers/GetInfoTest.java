446962315b0a2142f6f8e8393d4c91a39e7c4f05
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-05-03 14:14:32 +0400

    feature: added docs for citizen registration #269

--- services/bank/src/test/java/nc/unc/cs/services/bank/controllers/GetInfoTest.java
+++ services/bank/src/test/java/nc/unc/cs/services/bank/controllers/GetInfoTest.java
@@ -14,15 +14,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
-import org.springframework.restdocs.request.PathParametersSnippet;
-import org.springframework.restdocs.request.RequestParametersSnippet;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -55,7 +51,7 @@ public class GetInfoTest {
           GetInfoTest.TAX_ID_DESCR
       };
 
-//  private static final ResponseFieldsSnippet PAYMENT_REQUEST_RESP = responseFields(GetInfoTest.PAYMENT_REQUEST_DESCR);
+  //  private static final ResponseFieldsSnippet PAYMENT_REQUEST_RESP = responseFields(GetInfoTest.PAYMENT_REQUEST_DESCR);
   private static final ResponseFieldsSnippet PAYMENT_REQUESTS_RESP =
       responseFields(fieldWithPath("[]").description("PaymentRequests"))
           .andWithPrefix("[].", GetInfoTest.PAYMENT_REQUEST_DESCR);
