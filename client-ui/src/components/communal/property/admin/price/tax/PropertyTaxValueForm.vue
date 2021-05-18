fe498409b246f827fe159676c49aa0edb61619a0
Spec163 <45912087+Spec163@users.noreply.github.com>
2021-05-18 20:31:35 +0400

    feature: region validation #326

--- client-ui/src/components/communal/property/admin/price/tax/PropertyTaxValueForm.vue
+++ client-ui/src/components/communal/property/admin/price/tax/PropertyTaxValueForm.vue
@@ -4,7 +4,7 @@
     <v-text-field
         type="text"
         label="Введите название региона"
-        v-model="region"
+        v-model.trim="region"
         hide-details="auto"
         :error-messages="regionErrors"
         required
@@ -70,7 +70,7 @@ export default {
       required,
       minLength: minLength(2),
       maxLength: maxLength(40),
-      alpha: val => /^[а-яё][а-яё-]*$/i.test(val)
+      alpha: val => /^[а-яё][а-яё-]+[ ]+(область|край|округ)+$/i.test(val)
     },
     pricePerSquareMeter: {
       required,
@@ -99,7 +99,7 @@ export default {
       if (!this.$v.region.$dirty) return errors
       !this.$v.region.minLength && errors.push('Название региона должно состоять из не менее, чем из 2 букв!')
       !this.$v.region.maxLength && errors.push('Название региона должно состоять из не более, чем из 40 букв!')
-      !this.$v.region.alpha && errors.push('Неверный формат региона! Пример: "Московская", "Ямало-Ненецкий"')
+      !this.$v.region.alpha && errors.push('Неверный формат региона! Пример: "Московская область", "Ямало-Ненецкий округ"')
       !this.$v.region.required && errors.push('Это обязательное поле!')
       return errors
     },
