b7dd00122e7dd4b08bcd78f5f90453ac701b6a68
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-18 22:22:52 +0400

    ci: verbose docker build
    Took 6 minutes

--- .circleci/build_docker_images.sh
+++ .circleci/build_docker_images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+set -x
 TAG=branch_${CIRCLE_BRANCH//\//_}.latest
 DEV_TAG=branch_develop.latest
 docker_hub_login() {
@@ -39,15 +41,15 @@ build_service() {
   docker_hub_login
   docker pull "${IMAGE_NAME}:${TAG}" || true
   if [[ "$(docker images -q ${IMAGE_NAME}:${TAG} 2> /dev/null)" == "" ]]; then
-    CACHE_FROM="--cache-from ${IMAGE_NAME}:${TAG}"
+    CACHE_FROM=--cache-from "${IMAGE_NAME}:${TAG}"
   else
     docker pull "${IMAGE_NAME}:${DEV_TAG}" || true
     if [[ "$(docker images -q ${IMAGE_NAME}:${DEV_TAG} 2> /dev/null)" == "" ]]; then
-      CACHE_FROM="--cache-from ${IMAGE_NAME}:${DEV_TAG}"
+      CACHE_FROM=--cache-from "${IMAGE_NAME}:${DEV_TAG}"
     else
       docker pull "${IMAGE_NAME}:latest" || true
       if [[ "$(docker images -q ${IMAGE_NAME}:latest 2> /dev/null)" == "" ]]; then
-        CACHE_FROM="--cache-from ${IMAGE_NAME}:latest"
+        CACHE_FROM=--cache-from "${IMAGE_NAME}:latest"
       fi
     fi
   fi
