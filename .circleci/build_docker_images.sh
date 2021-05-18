ee418b86182756bb0df61a0a70bc4902b4c8b9fc
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-18 22:36:01 +0400

    ci: fix docker baseline names
    Took 9 minutes

--- .circleci/build_docker_images.sh
+++ .circleci/build_docker_images.sh
@@ -17,19 +17,24 @@ build_baseline() {
   docker_hub_login
   docker pull "${IMAGE_NAME}:${TAG}" || true
   if [[ "$(docker images -q ${IMAGE_NAME}:${TAG} 2> /dev/null)" == "" ]]; then
-    CACHE_FROM="--cache-from ${IMAGE_NAME}:${TAG}"
+    CACHE_FROM=--cache-from "${IMAGE_NAME}:${TAG}"
   else
     docker pull "${IMAGE_NAME}:${DEV_TAG}" || true
     if [[ "$(docker images -q ${IMAGE_NAME}:${DEV_TAG} 2> /dev/null)" == "" ]]; then
-      CACHE_FROM="--cache-from ${IMAGE_NAME}:${DEV_TAG}"
+      CACHE_FROM=--cache-from "${IMAGE_NAME}:${DEV_TAG}"
     else
       docker pull "${IMAGE_NAME}:latest" || true
       if [[ "$(docker images -q ${IMAGE_NAME}:latest 2> /dev/null)" == "" ]]; then
-        CACHE_FROM="--cache-from ${IMAGE_NAME}:latest"
+        CACHE_FROM=--cache-from "${IMAGE_NAME}:latest"
       fi
     fi
   fi
-  docker build "${CACHE_FROM}" -f "${DOCKER_FILE}" -t "${IMAGE_NAME}:${TAG}" -t "${IMAGE_NAME}:latest" .
+  if [[ -z "${CACHE_FROM}" ]]; then
+    docker build -f "${DOCKER_FILE}" -t "${IMAGE_NAME}:${TAG}" -t "${IMAGE_NAME}:latest" .
+  else
+    docker build ${CACHE_FROM} -f "${DOCKER_FILE}" -t "${IMAGE_NAME}:${TAG}" -t "${IMAGE_NAME}:latest" .
+  fi
+  docker build ${CACHE_FROM} -f "${DOCKER_FILE}" -t "${IMAGE_NAME}:${TAG}" -t "${IMAGE_NAME}:latest" .
   docker push "${IMAGE_NAME}:${TAG}"
   docker push "${IMAGE_NAME}:latest"
 }
@@ -53,7 +58,11 @@ build_service() {
       fi
     fi
   fi
-  docker build "${CACHE_FROM}" -f backend.production.Dockerfile --build-arg "PROJECT=${PROJECT}" -t "${IMAGE_NAME}:${TAG}" -t "${HEROKU_IMAGE_NAME}/web" -t "${IMAGE_NAME}:latest" .
+  if [[ -z "${CACHE_FROM}" ]]; then
+    docker build -f backend.production.Dockerfile --build-arg "PROJECT=${PROJECT}" -t "${IMAGE_NAME}:${TAG}" -t "${HEROKU_IMAGE_NAME}/web" -t "${IMAGE_NAME}:latest" .
+  else
+    docker build ${CACHE_FROM} -f backend.production.Dockerfile --build-arg "PROJECT=${PROJECT}" -t "${IMAGE_NAME}:${TAG}" -t "${HEROKU_IMAGE_NAME}/web" -t "${IMAGE_NAME}:latest" .
+  fi
   docker push "${IMAGE_NAME}:${TAG}"
   docker push "${IMAGE_NAME}:latest"
   docker_heroku_login
@@ -67,19 +76,23 @@ build_ui() {
   docker_hub_login
   docker pull "${IMAGE_NAME}:${TAG}" || true
   if [[ "$(docker images -q ${IMAGE_NAME}:${TAG} 2> /dev/null)" == "" ]]; then
-    CACHE_FROM="--cache-from ${IMAGE_NAME}:${TAG}"
+    CACHE_FROM=--cache-from "${IMAGE_NAME}:${TAG}"
   else
     docker pull "${IMAGE_NAME}:${DEV_TAG}" || true
     if [[ "$(docker images -q ${IMAGE_NAME}:${DEV_TAG} 2> /dev/null)" == "" ]]; then
-      CACHE_FROM="--cache-from ${IMAGE_NAME}:${DEV_TAG}"
+      CACHE_FROM=--cache-from "${IMAGE_NAME}:${DEV_TAG}"
     else
       docker pull "${IMAGE_NAME}:latest" || true
       if [[ "$(docker images -q ${IMAGE_NAME}:latest 2> /dev/null)" == "" ]]; then
-        CACHE_FROM="--cache-from ${IMAGE_NAME}:latest"
+        CACHE_FROM=--cache-from "${IMAGE_NAME}:latest"
       fi
     fi
   fi
-  docker build "${CACHE_FROM}" -f client-ui/prod.Dockerfile -t "${IMAGE_NAME}:${TAG}" -t "${HEROKU_IMAGE_NAME}/web" -t "${IMAGE_NAME}:latest" ./client-ui
+  if [[ -z "${CACHE_FROM}" ]]; then
+    docker build -f client-ui/prod.Dockerfile -t "${IMAGE_NAME}:${TAG}" -t "${HEROKU_IMAGE_NAME}/web" -t "${IMAGE_NAME}:latest" ./client-ui
+  else
+    docker build ${CACHE_FROM} -f client-ui/prod.Dockerfile -t "${IMAGE_NAME}:${TAG}" -t "${HEROKU_IMAGE_NAME}/web" -t "${IMAGE_NAME}:latest" ./client-ui
+  fi
   docker push "${IMAGE_NAME}:${TAG}"
   docker push "${IMAGE_NAME}:latest"
   docker_heroku_login
