f6051da4a8516ff7b3e9ed7bb20cbffe92afba5f
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-18 22:39:12 +0400

    ci: fix docker baseline names
    Took 3 minutes

--- .circleci/build_docker_images.sh
+++ .circleci/build_docker_images.sh
@@ -17,15 +17,15 @@ build_baseline() {
   docker_hub_login
   docker pull "${IMAGE_NAME}:${TAG}" || true
   if [[ "$(docker images -q ${IMAGE_NAME}:${TAG} 2> /dev/null)" == "" ]]; then
-    CACHE_FROM=--cache-from "${IMAGE_NAME}:${TAG}"
+    CACHE_FROM="--cache-from ${IMAGE_NAME}:${TAG}"
   else
     docker pull "${IMAGE_NAME}:${DEV_TAG}" || true
     if [[ "$(docker images -q ${IMAGE_NAME}:${DEV_TAG} 2> /dev/null)" == "" ]]; then
-      CACHE_FROM=--cache-from "${IMAGE_NAME}:${DEV_TAG}"
+      CACHE_FROM="--cache-from ${IMAGE_NAME}:${DEV_TAG}"
     else
       docker pull "${IMAGE_NAME}:latest" || true
       if [[ "$(docker images -q ${IMAGE_NAME}:latest 2> /dev/null)" == "" ]]; then
-        CACHE_FROM=--cache-from "${IMAGE_NAME}:latest"
+        CACHE_FROM="--cache-from ${IMAGE_NAME}:latest"
       fi
     fi
   fi
@@ -46,15 +46,15 @@ build_service() {
   docker_hub_login
   docker pull "${IMAGE_NAME}:${TAG}" || true
   if [[ "$(docker images -q ${IMAGE_NAME}:${TAG} 2> /dev/null)" == "" ]]; then
-    CACHE_FROM=--cache-from "${IMAGE_NAME}:${TAG}"
+    CACHE_FROM="--cache-from ${IMAGE_NAME}:${TAG}"
   else
     docker pull "${IMAGE_NAME}:${DEV_TAG}" || true
     if [[ "$(docker images -q ${IMAGE_NAME}:${DEV_TAG} 2> /dev/null)" == "" ]]; then
-      CACHE_FROM=--cache-from "${IMAGE_NAME}:${DEV_TAG}"
+      CACHE_FROM="--cache-from ${IMAGE_NAME}:${DEV_TAG}"
     else
       docker pull "${IMAGE_NAME}:latest" || true
       if [[ "$(docker images -q ${IMAGE_NAME}:latest 2> /dev/null)" == "" ]]; then
-        CACHE_FROM=--cache-from "${IMAGE_NAME}:latest"
+        CACHE_FROM="--cache-from ${IMAGE_NAME}:latest"
       fi
     fi
   fi
@@ -76,15 +76,15 @@ build_ui() {
   docker_hub_login
   docker pull "${IMAGE_NAME}:${TAG}" || true
   if [[ "$(docker images -q ${IMAGE_NAME}:${TAG} 2> /dev/null)" == "" ]]; then
-    CACHE_FROM=--cache-from "${IMAGE_NAME}:${TAG}"
+    CACHE_FROM="--cache-from ${IMAGE_NAME}:${TAG}"
   else
     docker pull "${IMAGE_NAME}:${DEV_TAG}" || true
     if [[ "$(docker images -q ${IMAGE_NAME}:${DEV_TAG} 2> /dev/null)" == "" ]]; then
-      CACHE_FROM=--cache-from "${IMAGE_NAME}:${DEV_TAG}"
+      CACHE_FROM="--cache-from ${IMAGE_NAME}:${DEV_TAG}"
     else
       docker pull "${IMAGE_NAME}:latest" || true
       if [[ "$(docker images -q ${IMAGE_NAME}:latest 2> /dev/null)" == "" ]]; then
-        CACHE_FROM=--cache-from "${IMAGE_NAME}:latest"
+        CACHE_FROM="--cache-from ${IMAGE_NAME}:latest"
       fi
     fi
   fi
