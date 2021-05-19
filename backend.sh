bd4c5c1102b33c387963357355c67cbc545f3d6f
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-19 12:47:37 +0400

    ci: docker - configure memory options for investigate
    Took 14 minutes

--- backend.sh
+++ backend.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env sh
-JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xms${APP_XMS} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m -XX:MaxDirectMemorySize=50m"
+#!/usr/bin/env bash
+if [[ -z "${APP_XMX}" ]]; then
+    export APP_XMX=248M
+fi
+if [[ -z "${APP_XMS}" ]]; then
+    export APP_XMS=100M
+fi
+JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointer -XX:+UnlockDiagnosticVMOptions -Xms${APP_XMX} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m"
+java -XX:+PrintFlagsFinal
 java org.springframework.boot.loader.JarLauncher  -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} ${JAVA_TOOL_OPTIONS}
