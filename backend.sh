116b42c05246d1cf4e4a4406ce63586d031a5afe
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-19 12:58:13 +0400

    ci: apps - configure number of threads per each app to reduce memory usase
    Took 11 minutes

--- backend.sh
+++ backend.sh
@@ -5,6 +5,11 @@ fi
 if [[ -z "${APP_XMS}" ]]; then
     export APP_XMS=100M
 fi
-JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointer -XX:+UnlockDiagnosticVMOptions -Xms${APP_XMX} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m"
+echo "------- [DEFAULT JAVA OPTIONS] -------"
 java -XX:+PrintFlagsFinal
+echo "------- [CONFIGURED JAVA OPTIONS] -------"
+JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseSerialGC -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointer -XX:+UnlockDiagnosticVMOptions -Xms${APP_XMX} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m -XX:MaxRAM=72m"
+java -XX:+PrintFlagsFinal ${JAVA_TOOL_OPTIONS}
+echo "------- [APPLICATION STARTED] -------"
 java org.springframework.boot.loader.JarLauncher  -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} ${JAVA_TOOL_OPTIONS}
+echo "------- [APPLICATION FINISHED] -------"
