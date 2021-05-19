1f7edc17ec030c42442b826703f083ebb56b171d
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-19 15:05:01 +0400

    ci: circleci/docker move JAVA options before app main class
    Took 8 minutes

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseSerialGC -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:MinHeapFreeRatio=20 XX:MaxHeapFreeRatio=40 -Xms${APP_XMX} -Xmx${APP_XMX} -Xss256K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m -XX:MaxRAM=64m -XX:MaxPermSize=64M -server -Djava.util.concurrent.ForkJoinPool.common.parallelism=4 -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
-JAVA_OPTS=${JAVA_TOOL_OPTIONS}
+JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseSerialGC -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:MinHeapFreeRatio=20 XX:MaxHeapFreeRatio=40 -Xms${APP_XMX} -Xmx${APP_XMX} -Xss256K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m -XX:MaxRAM=64m -XX:MaxPermSize=64M -server -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
+APP_ARGS="-Djava.util.concurrent.ForkJoinPool.common.parallelism=4 -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT}"
 echo "------- [APPLICATION STARTED] -------"
-java org.springframework.boot.loader.JarLauncher  -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} ${JAVA_OPTS}
+java ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher  ${APP_ARGS}
 echo "------- [APPLICATION FINISHED] -------"
