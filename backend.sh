b671608c3d56ae5743faf360eecafd542bc73508
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-19 13:23:46 +0400

    ci: circleci/docker fix jvm option - UseCompressedClassPointers
    Took 3 minutes

--- backend.sh
+++ backend.sh
@@ -2,7 +2,7 @@
 echo "------- [DEFAULT JAVA OPTIONS] -------"
 java -XX:+PrintFlagsFinal
 echo "------- [CONFIGURED JAVA OPTIONS] -------"
-JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseSerialGC -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointer -XX:+UnlockDiagnosticVMOptions -Xms${APP_XMX} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m -XX:MaxRAM=72m"
+JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseSerialGC -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:+UnlockDiagnosticVMOptions -Xms${APP_XMX} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m -XX:MaxRAM=72m"
 java -XX:+PrintFlagsFinal ${JAVA_TOOL_OPTIONS}
 echo "------- [APPLICATION STARTED] -------"
 java org.springframework.boot.loader.JarLauncher  -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} ${JAVA_TOOL_OPTIONS}
