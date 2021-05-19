6931b98973dc19e35d3bc30c5c3041eb8c9127d3
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-19 16:26:50 +0400

    ci: migrate logging service to use Eureika to find config service
    Took 15 minutes

--- backend.sh
+++ backend.sh
@@ -2,8 +2,8 @@
 
 JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseSerialGC -XX:+UseCGroupMemoryLimitForHeap -XX:+UseCompressedOops -XX:+UseCompressedClassPointers -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -Xms${APP_XMX} -Xmx${APP_XMX} -Xss256K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -XX:MaxDirectMemorySize=36m -XX:MaxRAM=64m -XX:MaxPermSize=64M -server -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 APP_ARGS="-Djava.util.concurrent.ForkJoinPool.common.parallelism=4 -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} -Dspring.profiles.active=${PROFILE} --spring.profiles.active=${PROFILE}"
-echo "------- [ENV] -------"
-env
+#echo "------- [ENV] -------"
+#env
 echo "------- [APPLICATION STARTED] -------"
 java ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher  ${APP_ARGS}
 echo "------- [APPLICATION FINISHED] -------"
