fc4b806026fe74899da8c47b7f60f9f829688b83
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2021-05-19 11:56:10 +0400

    ci: docker - fix memory usage for all apps to allocate no more that 512mb RAM
    Took 9 minutes

--- backend.sh
+++ backend.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-java org.springframework.boot.loader.JarLauncher -Xms${APP_XMS} -Xmx${APP_XMX} -Xss512K -XX:MaxMetaspaceSize=140m -XX:MaxDirectMemorySize=50m -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT}
+JAVA_TOOL_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xms${APP_XMS} -Xmx${APP_XMX} -Xss512K -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m -XX:MaxDirectMemorySize=50m"
+java org.springframework.boot.loader.JarLauncher  -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} ${JAVA_TOOL_OPTIONS}
