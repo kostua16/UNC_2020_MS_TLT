f52a0c6c6f585995bff3b37585ea5bd813597dc7
Restyled.io <commits@restyled.io>
2021-05-20 13:54:21 +0000

    Restyled by google-java-format

--- services/proxy/src/main/java/nc/unc/cs/gateway/PingService.java
+++ services/proxy/src/main/java/nc/unc/cs/gateway/PingService.java
@@ -1,10 +1,10 @@
 package nc.unc.cs.gateway;
 
-import java.util.ArrayList;
-import java.util.List;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -46,21 +46,18 @@ public class PingService {
       for (InstanceInfo instance : application.getInstances()) {
         String result = "";
         try {
-          final ResponseEntity<String> resp = this.rest.getForEntity(instance.getHealthCheckUrl(), String.class);
-          result = String.format(
-              "%s/%s responded with %s code",
-              instance.getId(),
-              instance.getHostName(),
-              resp.getStatusCode().value()
-          );
+          final ResponseEntity<String> resp =
+              this.rest.getForEntity(instance.getHealthCheckUrl(), String.class);
+          result =
+              String.format(
+                  "%s/%s responded with %s code",
+                  instance.getId(), instance.getHostName(), resp.getStatusCode().value());
           log.info(result);
         } catch (RestClientException exception) {
-          result = String.format(
-              "%s/%s failed with %s message",
-              instance.getId(),
-              instance.getHostName(),
-              exception.getMessage()
-          );
+          result =
+              String.format(
+                  "%s/%s failed with %s message",
+                  instance.getId(), instance.getHostName(), exception.getMessage());
           log.error(result);
         }
         response.add(result);
